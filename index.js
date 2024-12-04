8b987f26b2f4c6dd3c34a63a5718975d530fe381
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-12-04 10:43:53 +0530

    added demo code

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("he")
+const figlet = require("figlet");
+const express = require('express');
+const port = 5000;
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`, server.port);
+});
+
+figlet.text(
+    "Gamedemons",
+    {
+      font: "epic",
+      whitespaceBreak: true,
+    },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+    }
+  );
