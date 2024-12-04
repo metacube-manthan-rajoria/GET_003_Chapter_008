9eb5b4faa7e7c0d28b5eace25675a908695b8c93
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2024-12-04 11:00:29 +0530

    commented some code

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const figlet = require("figlet");
-const express = require('express');
+const express = require("express");
 const port = 5000;
 const app = express();
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`, server.port);
+	console.log(`Server started on port ${port}`);
 });
+// var listener = app.listen(8888, function(){
+//   console.log('Listening on port ' + listener.address().port);
+// });
 
 figlet.text(
-    "Gamedemons",
-    {
-      font: "epic",
-      whitespaceBreak: true,
-    },
-    function (err, data) {
-      if (err) {
-        console.log("Something went wrong...");
-        console.dir(err);
-        return;
-      }
-      console.log(data);
-    }
-  );
+	"Gamedemons",
+	{
+		font: "epic",
+		whitespaceBreak: true,
+	},
+	function (err, data) {
+		if (err) {
+			console.log("Something went wrong...");
+			console.dir(err);
+			return;
+		}
+		console.log(data);
+	}
+);
